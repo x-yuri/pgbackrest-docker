ded4d67554bca0ae79f426b804ab0acc024591c4
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2022-05-23 05:15:14 -0500

    ~ disable mail

--- back-up.sh
+++ back-up.sh
@@ -10,11 +10,12 @@ pgbackrest --stanza=db --log-level-console info "$@" backup >"$tmp" 2>&1 || e=$?
 cat "$tmp"
 if [ "$e" -gt 0 ]; then
     msg=$(printf 'Output:\n\n%s' "`cat "$tmp"`")
-    curl -s --user "api:$MAILGUN_KEY" \
-        https://api.mailgun.net/v3/"$MAILGUN_DOMAIN"/messages \
-        -F from="$MAILGUN_FROM" \
-        -F to="$MAILGUN_TO" \
-        -F subject='There was an error during backup' \
-        -F text="$msg"
+    printf "%s mail\n%s" -- "$msg"
+    # curl -s --user "api:$MAILGUN_KEY" \
+    #     https://api.mailgun.net/v3/"$MAILGUN_DOMAIN"/messages \
+    #     -F from="$MAILGUN_FROM" \
+    #     -F to="$MAILGUN_TO" \
+    #     -F subject='There was an error during backup' \
+    #     -F text="$msg"
 fi
 exit "$e"
