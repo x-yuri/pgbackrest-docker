a2865545548fc7956e863bc3b0d4f083499b9b53
Yuri Kanivetsky <yuri.kanivetsky@gmail.com>
2022-05-31 21:56:00 +0300

    Add samba container

--- db.sh
+++ db.sh
@@ -4,6 +4,7 @@ mkdir -p ~postgres/.ssh
 cp id_rsa authorized_keys known_hosts ~postgres/.ssh
 chown -R postgres: ~postgres/.ssh
 /usr/sbin/sshd
+wait4ports tcp://"`echo "$STORAGE_BOX" | cut -d/ -f3`":139
 if [ "${DONT_START_DB-}" ]; then
     exec sleep infinity
 else
